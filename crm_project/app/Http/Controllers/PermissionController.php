d8811e1d79ccc0212bda11166b417e2696920bbb
FarisHollow <mirfaris79@gmail.com>
2023-10-11 00:59:57 +0600

    permission

--- crm_project/app/Http/Controllers/PermissionController.php
+++ crm_project/app/Http/Controllers/PermissionController.php
@@ -9,14 +9,11 @@ class PermissionController extends Controller
 {
     public function store(Request $request)
     {
-        // Retrieve the selected values (1 for "Yes," 0 for "No") from the form.
         $noticeValue = $request->input('notice') === '1' ? true : false;
         $leadValue = $request->input('lead') === '1' ? true : false;
 
-        // Find the existing record or create a new one if it doesn't exist.
         $permission = Permission::firstOrNew([]);
 
-        // Update the 'notice' and 'lead' columns of the record with the new boolean values.
         $permission->notice = $noticeValue;
         $permission->lead = $leadValue;
 
